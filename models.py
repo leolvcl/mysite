f29cda4a153b6888a1ec1741da045c20644e40a1
lion000000 <907647971@qq.com>
2019-03-12 14:04:28 +0800

    提交1“

---
+++ models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=150) # 博客标题
+    body = models.TextField() # 正文
+    timestamp = models.DateTimeField() # 创建时间
+
